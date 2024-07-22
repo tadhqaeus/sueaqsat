64eae3d70080d1b4c9f2b6ef78ffb89a05338832
tadhqaeus <174170981+tadhqaeus@users.noreply.github.com>
2024-07-22 23:29:53 +0800

    ◦ second commit ◦
    ‣ implemented the basic/necessary functions of a todo list
    ‣ added some animations

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+const taskInput = document.getElementById("input-task");
+const taskList = document.getElementById("todo-list");
+
+function addTask() {
+	const taskValue = taskInput.value.trim();
+
+	if(taskValue) {
+		const newTask = document.createElement("li");
+
+		newTask.innerHTML = `
+			<i class="material-icons cb">check_box_outline_blank</i>
+			<p>${taskValue}</p>
+			<i class="material-icons remove-task">close</i>
+		`;
+		taskList.appendChild(newTask);
+		taskInput.value = '';
+	} else {
+		taskInput.classList.add("empty");
+		setTimeout(() => {
+			taskInput.classList.remove("empty");
+		}, 300);
+	}
+}
+
+taskList.addEventListener("click", function(event) {
+	const target = event.target;
+
+	if (target.classList.contains("cb")) {
+		if (target.classList.contains("checked")) {
+			target.classList.remove("checked");
+			target.textContent = "check_box_outline_blank";
+		} else {
+			target.classList.add("checked");
+			target.textContent = "check_box";
+		}
+	}
+
+	if (target.classList.contains("remove-task")) {
+		target.parentElement.remove();
+	}
+});
 
